73ece172a47bcdcd838404e6c0ff19fd702d50cc
shiyas03 <shiyastofficial@gmail.com>
2023-09-06 10:05:43 +0530

    data collected in weather component

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ServiceService } from '../services/service.service';
+import { Subscription } from 'rxjs';
+import { WeatherData } from '../shared/weather.interface';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css']
 })
-export class WeatherComponent {
+export class WeatherComponent implements OnInit, OnDestroy {
+
+  city!: string
+  data!: WeatherData
+  subscription!: Subscription
+
+  constructor(private services: ServiceService) { }
+
+  ngOnInit(): void {
+    this.city = 'london'
+    this.fetchWeather(this.city)
+  }
+
+  fetchWeather(city: string) {
+    this.subscription = this.services.fetchWeatherData(city).subscribe(
+      (data) => {
+        this.data = data
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) this.subscription.unsubscribe()
+  }
 }
